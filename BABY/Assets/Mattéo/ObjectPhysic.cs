0fab1428a5fd287ed8e839597c1d86553e315c32
Mattéo Chiarenza <matteo.chiarenza.pro@gmail.com>
2024-01-27 13:05:47 +0100

    Ajout d'une sélection aléatoire de l'angle de lancer des objets + Ajout d'une variable public permettant de régler la puissance de la force de projection

--- BABY/Assets/Mattéo/ObjectPhysic.cs
+++ BABY/Assets/Mattéo/ObjectPhysic.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class ObjectPhysic : MonoBehaviour
 {
     new Rigidbody rigidbody;
+    [Range(0f, 100f)] public float forcePower = 50;
 
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rigidbody = GetComponent<Rigidbody>();                                                          
         StartCoroutine(physicsProjection());
     }
 
     public IEnumerator physicsProjection() {
-        rigidbody.AddForce(0, 100, 0);
-        yield return new WaitForSeconds(1);
-        rigidbody.AddTorque(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
-        transform.rotation.Set(Random.Range(0, 360), transform.rotation.y, transform.rotation.z, transform.rotation.w);
-        rigidbody.AddForce(Random.Range(100, 500), 0, Random.Range(100, 500));
-        yield return null;
+        rigidbody.AddForce(0, 100, 0);                                                                  // Montée de l'objet
+        yield return new WaitForSeconds(1);                                                             // On attend 1 sec
+        rigidbody.AddTorque(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f));    // Ajout d'une force de rotation
+        Vector3 force = new Vector3(Random.Range(-50f, 50f), 0, Random.Range(-50f, 50f));
+        force = force.normalized * forcePower;
+        rigidbody.AddForce(force, ForceMode.VelocityChange);                                            // Projection de l'objet
+        yield return null;                                                                              // Fin de la gestion de la physique
     }
 }
