7cc7c5e0243e42b0f79bbb77e8d3f0d9c1ec7575
Mattéo Chiarenza <matteo.chiarenza.pro@gmail.com>
2024-01-27 13:46:09 +0100

    Prototypage du reset de position des objets

---
+++ BABY/Assets/Mattéo/ResetObjectPositionScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetObjectPositionScript : MonoBehaviour
+{
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+
+    void Start() {
+        this.originalPosition = this.transform.position;
+	    this.originalRotation = this.transform.rotation;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) /* Remplacer par la condition de clic sur l'objet */)
+        {
+            ResetPosition();
+        }
+    }
+
+    void ResetPosition() {
+        this.transform.position = this.originalPosition;
+	    this.transform.rotation = this.originalRotation;
+        Debug.Log("Reset de la position de " + gameObject.name + ".");
+    }
+}
